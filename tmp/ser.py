dd5a8d19e141cb2460b8c75834cb357511d9fb38
koddev <cagcode@gmail.com>
2020-03-31 14:04:52 +0300

    gear

--- tmp/ser.py
+++ tmp/ser.py
@@ -1,55 +1,87 @@
-from vidgear.gears import VideoGear
-from vidgear.gears import NetGear
-from vidgear.gears import WriteGear
-import cv2
+#! /usr/bin/python
 
-from time import sleep
+# pyrtsp - RTSP test server hack
+# Copyright (C) 2013  Robert Swain <robert.swain@gmail.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-#writer = WriteGear(output_filename = 'Output.mp4') #Define writer
+import gi
+import time
+import asyncio
 
-vid_path="/mnt/sd/istiklal.mp4"
-# vid_path="/home/cc/PycharmProjects/assets/istiklal1.mp4"
-# stream = VideoGear(source=vid_path).start()  # Open any video stream
-stream = cv2.VideoCapture(vid_path)  # Open any video stream
+gi.require_version('Gst', '1.0')
+gi.require_version('GstRtspServer', '1.0')
+gi.require_version('GstVideo', '1.0')
+from gi.repository import GLib, Gst, GstVideo, GstRtspServer
 
-options = {'flag': 0, 'copy': False, 'track': False}
-
-# change following IP address '192.168.x.xxx' with client's IP address
-server = NetGear(address='192.168.1.25', port='5454', protocol='tcp', pattern=0, receive_mode=False, logging=True,
-                 **options)  # Define netgear server at your system IP address.
-
-# infinite loop until [Ctrl+C] is pressed
-encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-while True:
-
-
-    try:
-        frame = stream.read()
-        # read frames
-
-
-
-        # check if frame is None
-        if frame is None:
-            # if True break the infinite loop
-            break
-
-        # do something with frame here
-
-        # send frame to server
-
-        # sleep(1/90)
-
-        result, encimg = cv2.imencode('.jpg', frame, encode_param)
-        server.send(encimg)
+mainloop = GLib.MainLoop()
+loopAsync = asyncio.get_event_loop()
 
 
-    except KeyboardInterrupt:
-        # break the infinite loop
-        break
+def startServer():
+    Gst.init(None)
 
-# safely close video stream
-stream.stop()
-# safely close server
-server.close()
-exit()
+    address = '192.168.196.80'
+    server = GstRtspServer.RTSPServer.new()
+    server.set_address(address)
+    server.set_service('8554')
+
+    auth = GstRtspServer.RTSPAuth()
+    token = GstRtspServer.RTSPToken()
+    token.set_string('media.factory.role', "user")
+    basic = GstRtspServer.RTSPAuth.make_basic("user", "pass")
+    auth.add_basic(basic, token)
+    server.set_auth(auth)
+
+    mounts = server.get_mount_points()
+
+    factory = GstRtspServer.RTSPMediaFactory()
+    permissions = GstRtspServer.RTSPPermissions()
+    permissions.add_permission_for_role("user", "media.factory.access", True)
+    permissions.add_permission_for_role("user", "media.factory.construct", True)
+    factory.set_permissions(permissions)
+
+    # factory.set_launch('( device=/dev/video1 ! video/x-raw,format=YUY2,width=1280,height=720,framerate=10/1 ! videoconvert ! x264enc ! video/x-h264,profile=high ! rtph264pay pt=96 name=pay0 )')
+    # factory.set_launch('( device=/dev/video1 ! ffmpegcolorspace ! video/x-raw,width=1920,framerate=5/1 ! ffmpegcolorspace ! directdrawsink -v )')
+    factory.set_launch('( filesrc location=istiklal1.mp4 ! qtdemux ! queue ! rtph264pay pt=96 name=pay0 )')
+    #factory.set_launch('(device=/dev/video0 ! video/x-raw-yuy,width=1280,height=720,framerate=10/1 ! videoconvert ! rtph264pay name=pay0 pt=96)')
+
+    mounts.add_factory("/test", factory)
+
+    server.attach(None)
+
+    print("stream ready at rtsp://user:pass@" + address + ":8554/test")
+
+    mainloop.run()
+
+
+def stopServer():
+    time.sleep(10)
+    mainloop.stop()
+    exit()
+
+
+def main():
+    asyncio.gather(startServer(), stopServer())
+
+
+if __name__ == "__main__":
+    s = time.perf_counter()
+    loopAsync.run_until_complete(main())
+
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
+    exit()
+
