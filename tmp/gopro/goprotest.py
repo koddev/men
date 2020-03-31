c360365a2824bf8432eaefb5af70549934ee039f
koddev <cagcode@gmail.com>
2020-03-31 19:11:28 +0300

    gear

--- tmp/gopro/goprotest.py
+++ tmp/gopro/goprotest.py
@@ -3,11 +3,12 @@ from goprocam import constants
 import cv2
 
 gopro = GoProCamera.GoPro()
-gopro.video_settings(res='1080p', fps='30')
-gopro.gpControlSet(constants.Stream.WINDOW_SIZE, constants.Stream.WindowSize.R720)
+# gopro.video_settings(res='1080p', fps='30')
+# gopro.gpControlSet(constants.Stream.WINDOW_SIZE, constants.Stream.WindowSize.R240)
 
 # gopro.streamSettings(constants.Stream.BitRate.B2_5Mbps,constants.Stream.WindowSize.R720_1by2Subsample)
-gopro.stream("udp://127.0.0.1:10000",quality='high')
+gopro.mode(constants.Mode.VideoMode,constants.Mode.)
+gopro.stream("udp://127.0.0.1:10000")
 
 
 
---
+++ tmp/gopro/livestr.py
@@ -0,0 +1,8 @@
+from goprocam import GoProCamera
+from goprocam import constants
+
+gpCam = GoProCamera.GoPro()
+gpCam.streamSettings(2400000,9)
+
+gpCam.livestream("start")
+gpCam.streamSettings(2400000,9)
