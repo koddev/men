4bd3f2c036c40483abb7cab3c26fb3db21b7b8d5
koddev <cagcode@gmail.com>
2020-04-01 16:36:17 +0300

    gear

--- rabbittest.py
+++ rabbittest.py
@@ -16,7 +16,7 @@ class CamFrameClass:
 
 
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('62.244.197.146',5550))
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.116.20',5550))
 channel = connection.channel()
 channel.queue_declare(queue='camFromBoard')
 
